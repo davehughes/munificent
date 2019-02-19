8704e47a54eef17077a8b103bacecc89ca12ed49
David Hughes <d@vidhughes.com>
2019-02-18 17:18:21 -0800

    Tests for vehicle locations parsing

--- test/test_collect.py
+++ test/test_collect.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 import unittest
 
-from munificent.collect import parse_prediction_points
+from munificent.collect import parse_prediction_points, parse_vehicle_locations
 
 from . import utils
 
@@ -12,3 +12,14 @@ class TestPredictionResultParsing(unittest.TestCase):
         prediction_points = utils.load_json_fixture('multiprediction.json')
         points = parse_prediction_points(prediction_points)
         self.assertEqual(299, len(points))
+
+    def test_vehicle_locations_fixture(self):
+        locations_fixture = utils.load_json_fixture('vehicle-locations.json')
+        locations = parse_vehicle_locations(locations_fixture)
+        self.assertEqual(11, len(locations))
+
+    def test_vehicle_locations_empty(self):
+        locations_fixture = utils.load_json_fixture('vehicle-locations.json')
+        del locations_fixture['vehicle']
+        locations = parse_vehicle_locations(locations_fixture)
+        self.assertEqual(0, len(locations))
